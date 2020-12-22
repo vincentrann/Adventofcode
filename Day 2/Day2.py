6455e7357451984e067b000e018a5006cd75bfce
vincentrann <vincentrann@github.com>
2020-12-22 16:08:33 -0500

    Day 2 both parts finished

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,6 +1,7 @@
+#part 1
 Passwords = open('Passwords.txt', 'r')
 correct = 0
-for x in Passwords:
+'''for x in Passwords:
     string = x.split(' ')
     min = string[0].split('-')[0]
     max = string[0].split('-')[1]
@@ -11,6 +12,16 @@ for x in Passwords:
     if count >= int(min) and count <= int(max):
         correct+=1
 
+print (correct)'''
+#part 2
+correct = 0
+for x in Passwords:
+    string = x.split(' ')
+    min = int(string[0].split('-')[0])
+    max = int(string[0].split('-')[1])
+    char = (string[1][0])
+    if (string[2][min-1] == char and string[2][max-1] != char) or (string[2][min-1] != char and string[2][max-1] == char):
+        correct+=1
 print (correct)
 
 
