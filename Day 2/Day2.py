cb550240974d130e29ce24ec7bea509475ccf3e3
vincentrann <vincentrann@github.com>
2020-12-22 12:41:53 -0500

    Day 2 Finished

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,4 +1,21 @@
-Passwords = open('Day 2/Passwords.txt', 'r')
-
+Passwords = open('Passwords.txt', 'r')
+correct = 0
 for x in Passwords:
-    print (x [0:' '])
+    string = x.split(' ')
+    min = string[0].split('-')[0]
+    max = string[0].split('-')[1]
+    count = 0
+    for y in string[2]:
+        if y == string[1][0]:
+            count+=1
+    if count >= int(min) and count <= int(max):
+        correct+=1
+
+print (correct)
+
+
+
+
+
+
+
