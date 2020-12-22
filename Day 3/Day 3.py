fcbad9ca4607d1163a14a98f8c0ca94dbcbfc835
vincentrann <vincentrann@github.com>
2020-12-22 18:04:47 -0500

    Day 3 DONE

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -1,1 +1,28 @@
-map = open('')
+#part 1
+data = open("Map.txt", "r")
+map = []
+
+for i in data:
+    map.append(i)
+
+border = len(map[0])-1
+
+
+def slope(xint,yint):
+    posx = 0
+    posy = 0
+    trees = 0
+
+    while posy < len(map):
+        if map[posy][posx] == "#":
+            trees+=1
+        posx += xint
+        posy +=yint
+        if posx >= border:
+            posx -=border
+    return trees
+
+print (slope(3,1))
+
+#part 2
+print (slope(1,1)*slope(3,1)*slope(5,1)*slope(7,1)*slope(1,2))
